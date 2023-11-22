0c397ca6db337eda243629509f358898a00aed5d
jombinoy24 <125428848+jombinoy24@users.noreply.github.com>
2023-11-22 09:37:48 +0530

    Add files via upload

--- cycle2/MainActivity.java
+++ cycle2/MainActivity.java
@@ -1,98 +1,36 @@
-package com.example.sjcet.calgrid;
+package com.example.sjcet.image;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.webkit.JavascriptInterface;
-import android.webkit.WebView;
 import android.widget.Button;
-import android.widget.GridLayout;
-import android.widget.TextView;
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private TextView textView;
-    private String currentInput = "";
-    private String operator = "";
-    private double firstOperand = 0;
-    private double secondOperand = 0;
-    private boolean isNewInput = true;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private Button toggleButton;
+    private boolean isImage1Displayed = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = findViewById(R.id.textView);
-        GridLayout gridLayout = findViewById(R.id.gridLayout);
-        String[] buttonLabels = {
-                "7", "8", "9", "/",
-                "4", "5", "6", "*",
-                "1", "2", "3", "-",
-                "C", "0", "=", "+"
-        };
-        for (String label : buttonLabels) {
-            Button button = new Button(this);
-            button.setText(label);
-            button.setTextSize(24);
-            button.setOnClickListener(this);
-            gridLayout.addView(button);
-        }
-    }
-    @Override
-    public void onClick(View v) {
-        Button button = (Button) v;
-        String buttonText = button.getText().toString();
-        switch (buttonText) {
-            case "=":
-                calculateResult();
-                break;
-            case "C":
-                clearInput();
-                break;
-            default:
-                handleInput(buttonText);
-                break;
-        }
-    }
-    private void handleInput(String input) {
-        if (isNewInput) {
-            currentInput = input;
-            isNewInput = false;
-        } else {
-            currentInput += input;
-        }
-        updateDisplay();
-    }
-    private void clearInput() {
-        currentInput = "";
-        operator = "";
-        firstOperand = 0;
-        secondOperand = 0;
-        isNewInput = true;
-        updateDisplay();
-    }
-    private void calculateResult() {
-        if (!isNewInput) {
-            String expression = currentInput;
-            try {
-                // Use JavaScript eval() to evaluate the expression
-                WebView webView = new WebView(this);
-                webView.getSettings().setJavaScriptEnabled(true);
-                webView.addJavascriptInterface(new Object() {
-                    @JavascriptInterface
-                    public void processHTML(String html) {
-                        // Process the result returned from JavaScript
-                        currentInput = html;
-                        isNewInput = true;
-                        updateDisplay();
-                    }
-                }, "Android");
-                webView.evaluateJavascript("javascript:Android.processHTML(eval('" + expression + "'))", null);
-            } catch (Exception e) {
-                currentInput = "Error: Invalid expression";
-                isNewInput = true;
-                updateDisplay();
+
+        imageView = findViewById(R.id.imageView);
+        toggleButton = findViewById(R.id.toggleButton);
+
+        toggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Toggle between the two images
+                if (isImage1Displayed) {
+                    imageView.setImageResource(R.drawable.image2);
+                } else {
+                    imageView.setImageResource(R.drawable.image1);
+                }
+                isImage1Displayed = !isImage1Displayed;
             }
-        }
-    }
-    private void updateDisplay() {
-        textView.setText(currentInput);
+        });
     }
 }
